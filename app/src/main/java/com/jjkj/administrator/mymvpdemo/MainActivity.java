7d960c1971b74db7fc884a8de84ea9e85fe85bfc
Guo JiaMing <guojiaming0911@163.com>
2018-04-19 13:40:06 +0800

    first commit

--- app/src/main/java/com/jjkj/administrator/mymvpdemo/MainActivity.java
+++ app/src/main/java/com/jjkj/administrator/mymvpdemo/MainActivity.java
@@ -3,6 +3,7 @@ package com.jjkj.administrator.mymvpdemo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Toast;
 
 /**
  * @author Administrator
@@ -15,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.main_btn).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                
+                Toast.makeText(MainActivity.this, "Toast", Toast.LENGTH_SHORT).show();
             }
         });
     }
